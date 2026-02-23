100b49c07149f259ffd425969acb468fc7deed77
Muhammad Naufal Rizki <mnflrizki@gmail.com>
2026-02-23 09:20:35 +0700

    Update deploy script for NVM and Node.js
    Set up NVM and updated deployment script for Node.js version 20. Commented out storage link command.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -e
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm use 20
+
 composer install --no-dev --optimize-autoloader
+
+npm install
+npm run build
 
 php artisan migrate --force
 
@@ -12,4 +20,5 @@ php artisan event:cache
 php artisan route:cache
 php artisan view:cache
 
-php artisan storage:link
+#Initialized once
+#php artisan storage:link
